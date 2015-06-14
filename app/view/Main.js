ee932cdb398c7ab98080a5324079bc6a7e5911bb
Daniel Sauble <djsauble@gmail.com>
2015-06-14 17:00:03 -0400

    Mocked the list, new, and edit views in code

--- app/view/Main.js
+++ app/view/Main.js
@@ -3,45 +3,168 @@ Ext.define('TodoApp.view.Main', {
     xtype: 'main',
     requires: [
         'Ext.TitleBar',
-        'Ext.Video'
+        'Ext.dataview.List',
+        'Ext.form.Panel',
+        'Ext.form.FieldSet',
+        'Ext.field.Radio'
     ],
     config: {
         tabBarPosition: 'bottom',
 
         items: [
             {
-                title: 'Welcome',
+                title: 'List',
                 iconCls: 'home',
 
-                styleHtmlContent: true,
-                scrollable: true,
+                xtype: 'list',
 
-                items: {
-                    docked: 'top',
-                    xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
+                store: {
+                    fields: ['name'],
+                    data: [
+                        {name: 'Eat food'},
+                        {name: 'Fetch slippers'},
+                        {name: 'Sit/stay'},
+                        {name: 'Chew bone'},
+                        {name: 'Go for a walk'},
+                        {name: 'Chase the mail carrier'},
+                        {name: ''},
+                        {name: 'Deleted items'},
+                        {name: 'Play with Sparky'},
+                        {name: 'Howl at the moon'},
+                        {name: 'Chase the cat'}
+                    ]
                 },
 
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
+                itemTpl: '{name}',
+
+                items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'Things to do',
+                        items: {
+                            align: 'right',
+                            text: 'Add'
+                        }
+                    }
+                ]
             },
             {
-                title: 'Get Started',
+                title: 'Create',
                 iconCls: 'action',
 
                 items: [
                     {
                         docked: 'top',
                         xtype: 'titlebar',
-                        title: 'Getting Started'
+                        title: 'Add item',
+                        items: {
+                            align: 'right',
+                            text: 'Save'
+                        }
                     },
                     {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
+                        xtype: 'formpanel',
+                        height: '100%',
+                        scrollable: true,
+
+                        items: [
+                            {
+                                xtype: 'fieldset',
+                                title: 'Description',
+                                items: {
+                                    xtype: 'textfield',
+                                    name: 'description'
+                                }
+                            },
+                            {
+                                xtype: 'fieldset',
+                                title: 'Image',
+                                items: {
+                                    xtype: 'button',
+                                    text: 'Select image…'
+                                }
+                            },
+                            {
+                                xtype: 'fieldset',
+                                title: 'Location',
+                                defaults: {
+                                    labelAlign: 'right',
+                                    labelWidth: '240px',
+                                    xtype: 'radiofield',
+                                    name: 'location'
+                                },
+                                items: [
+                                    {value: 'here', label: 'Current location'},
+                                    {value: 'home', label: 'Home'},
+                                    {value: 'work', label: 'Work'}
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                title: 'Edit',
+                iconCls: 'action',
+
+                items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'Edit item',
+                        items: {
+                            align: 'right',
+                            text: 'Save'
+                        }
+                    },
+                    {
+                        xtype: 'formpanel',
+                        height: '100%',
+                        scrollable: true,
+
+                        items: [
+                            {
+                                xtype: 'fieldset',
+                                title: 'Description',
+                                items: {
+                                    xtype: 'textfield',
+                                    name: 'description',
+                                    value: 'Chase the mail carrier'
+                                }
+                            },
+                            {
+                                xtype: 'fieldset',
+                                title: 'Image',
+                                items: [
+                                    {
+                                        xtype: 'panel',
+                                        width: '100%',
+                                        height: 156,
+                                        style: 'background: #AAAAAA'
+                                    },
+                                    {
+                                        xtype: 'button',
+                                        text: 'Remove image'
+                                    }
+                                ]
+                            },
+                            {
+                                xtype: 'fieldset',
+                                title: 'Location',
+                                defaults: {
+                                    labelAlign: 'right',
+                                    labelWidth: '240px',
+                                    xtype: 'radiofield',
+                                    name: 'location'
+                                },
+                                items: [
+                                    {value: 'home', label: 'Home', checked: true},
+                                    {value: 'work', label: 'Work'},
+                                    {value: 'other', label: 'Other'}
+                                ]
+                            }
+                        ]
                     }
                 ]
             }
